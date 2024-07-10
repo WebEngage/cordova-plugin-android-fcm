f37176a5d7f6f481baf43d6f0d88180013a7e591
akshay-engage <114734802+akshay-engage@users.noreply.github.com>
2024-07-10 13:13:51 +0530

    Code refactored and updated gradle version to latest

--- src/android/AndroidFcmPlugin.java
+++ src/android/AndroidFcmPlugin.java
@@ -2,16 +2,12 @@ package com.androidfcm.cordova;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaInterface;
-import org.apache.cordova.CordovaWebView;
-import androidx.annotation.NonNull;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.webengage.sdk.android.WebEngage;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -35,43 +31,33 @@ public class AndroidFcmPlugin extends CordovaPlugin {
 
     private void updateToken() {
         if (CustomFirebaseMessagingService.includesWebEngage()) {
-               FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
-                           @Override
-                           public void onComplete(@NonNull Task<String> task) {
-                                           try {
-                                               String token = task.getResult();
-                                               com.webengage.sdk.android.Logger.d(TAG, "Updating WebEngage FCM token: " + token);
-                                               com.webengage.sdk.android.WebEngage.get().setRegistrationID(token);
-                                           } catch (Exception e) {
-                                                 com.webengage.sdk.android.Logger.e(TAG, "FCM token error", e);
-                                           }
-                                       }
-                                   });
-
-
-            }
+            FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+                try {
+                    String token = task.getResult();
+                    com.webengage.sdk.android.Logger.d(TAG, "Updating WebEngage FCM token: " + token);
+                    WebEngage.get().setRegistrationID(token);
+                } catch (Exception e) {
+                    com.webengage.sdk.android.Logger.e(TAG, "FCM token error", e);
+                }
+            });
+        }
     }
 
 
     private void getToken(CallbackContext callbackContext) {
         if (callbackContext != null) {
-
-                FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<String> task) {
-                                            try {
-                                                String token = task.getResult();
-                                                 try {
-                                                                         callbackContext.success(token);
-                                                                     } catch (Throwable t) {
-                                                                         callbackContext.error(t.getMessage());
-                                                                     }
-                                            } catch (Exception e) {
-                                    com.webengage.sdk.android.Logger.e(TAG, "FCM token error", e);
-                                            }
-                                        }
-                                    });
-
+            FirebaseMessaging.getInstance().getToken().addOnCompleteListener(task -> {
+                try {
+                    String token = task.getResult();
+                    try {
+                        callbackContext.success(token);
+                    } catch (Throwable t) {
+                        callbackContext.error(t.getMessage());
+                    }
+                } catch (Exception e) {
+                    com.webengage.sdk.android.Logger.e(TAG, "FCM token error", e);
+                }
+            });
         }
     }
 }
